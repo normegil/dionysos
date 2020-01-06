5706cf6848dab51ad1da663e7c74b1587e297030
Olivier Ory <olivier.ory@uliege.be>
2020-01-06 13:39:16 +0100

    Web-UI - Basic vue setup

---
+++ website/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <h1>My Todo App!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+        }
+    }
+</script>
+
+<style>
+</style>
--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'development',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 };
