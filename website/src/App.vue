5cfc9bdb931e1ec503494181bc56566ca0c4a796
Olivier Ory <olivier.ory@uliege.be>
2020-01-07 10:04:08 +0100

    Web-UI - Configured Prettier & correct linting issues

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,19 +1,18 @@
 <template>
-    <div id="app">
-        <h1>My Todo App!</h1>
-        <p>{{test}}</p>
-    </div>
+  <div id="app">
+    <h1>My Todo App!</h1>
+    <p>{{ test }}</p>
+  </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
 
-    @Component
-    export default class App extends Vue {
-        test: string = "TSTest";
-    }
+@Component
+export default class App extends Vue {
+  test = "TSTest";
+}
 </script>
 
-<style>
-</style>
+<style></style>
--- website/src/index.ts
+++ website/src/index.ts
@@ -1,6 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
 new Vue({
-    render: h => h(App),
-}).$mount('#app')
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  render: h => h(App)
+}).$mount("#app");
