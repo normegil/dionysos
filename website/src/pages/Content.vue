14acd907a60cbfae29354da2efe1ffb78706c2da
Olivier Ory <olivier.ory@uliege.be>
2020-01-15 16:56:33 +0100

    WebUI - Add page title

--- website/src/pages/Content.vue
+++ website/src/pages/Content.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="content">
     <SearchField />
+    <div class="content__title-container">
+      <div class="content__title-background" />
+      <div class="content__title-text">
+        <h1>{{ $t("ui.menu.main.items") }}</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +25,28 @@ export default class Content extends Vue {}
   background-color: $color-white;
 
   overflow: auto;
+
+  &__title {
+    &-container {
+      overflow: hidden;
+      height: 9rem;
+      position: relative;
+    }
+
+    &-background {
+      background-image: url("../assets/images/items-title.jpg");
+      background-size: cover;
+      background-position: center;
+      filter: blur(8px);
+      height: 100%;
+    }
+
+    &-text {
+      color: $color-black;
+      position: absolute;
+      top: 50%;
+      left: 20rem;
+    }
+  }
 }
 </style>
