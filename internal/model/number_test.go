5aa42d1ca5e41948996bf38230fef815bf8bfe1d
Olivier Ory <olivier.ory@uliege.be>
2020-03-05 13:34:10 +0100

    Server - Tests - Natural

---
+++ internal/model/number_test.go
@@ -0,0 +1,37 @@
+package model_test
+
+import (
+	"github.com/normegil/dionysos/internal/model"
+	"strconv"
+	"testing"
+)
+
+func TestNewNatural(t *testing.T) {
+	tests := []struct {
+		tested      int
+		expectError bool
+	}{
+		{
+			tested:      1,
+			expectError: false,
+		},
+		{
+			tested:      0,
+			expectError: false,
+		},
+		{
+			tested:      -1,
+			expectError: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(strconv.Itoa(test.tested), func(t *testing.T) {
+			_, err := model.NewNatural(test.tested)
+			if nil != err && !test.expectError {
+				t.Errorf("didn't expect an error for '%d': %w", test.tested, err)
+			}
+
+		})
+	}
+}
