c8c23c6f8c5e2fbefb20be840a0c68af745e79d7
Olivier Ory <olivier.ory@uliege.be>
2020-03-05 15:38:20 +0100

    Server - Tests - NilRoleDAO

---
+++ internal/security/role_test.go
@@ -0,0 +1,49 @@
+package security_test
+
+import (
+	"github.com/google/uuid"
+	"github.com/normegil/dionysos/internal/security"
+	"testing"
+)
+
+func TestNilRoleDAO_LoadByName(t *testing.T) {
+	tests := []struct {
+		name           string
+		availableRoles []*security.Role
+		searched       string
+		isNilRole      bool
+	}{
+		{
+			name:           "Nil Role",
+			availableRoles: []*security.Role{},
+			searched:       "",
+			isNilRole:      true,
+		},
+		{
+			name: "Classic Role",
+			availableRoles: []*security.Role{
+				{
+					ID:   uuid.New(),
+					Name: "testRole",
+				},
+			},
+			searched:  "testRole",
+			isNilRole: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			dao := security.NilRoleDAO{RoleDAO: security.Test_FakeRoleDAO{Roles: test.availableRoles}}
+			role, err := dao.LoadByName(test.searched)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			roleNil := security.RoleNil()
+			if (test.isNilRole && roleNil != *role) || (!test.isNilRole && roleNil == *role) {
+				t.Errorf("Not expected result {isNill:%t;got:%+v}", test.isNilRole, role)
+			}
+		})
+	}
+}
--- internal/security/testing.go
+++ internal/security/testing.go
@@ -1,1 +1,14 @@
 package security
+
+type Test_FakeRoleDAO struct {
+	Roles []*Role
+}
+
+func (d Test_FakeRoleDAO) LoadByName(name string) (*Role, error) {
+	for _, role := range d.Roles {
+		if role.Name == name {
+			return role, nil
+		}
+	}
+	return nil, nil
+}
